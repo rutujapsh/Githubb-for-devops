852b58e1921d46d4ceb0aca951895aca7d06fa39
Siddhika <Siddhika123@gmail.com>
2025-07-17 18:32:03 +0530

    added testing files

---
+++ main.java
@@ -0,0 +1,11 @@
+class A{
+    public void print(){
+        System.out.println("Hello new world");
+    }
+}
+class main{
+    public static void main(String args[]){
+        A a = new A();
+        a.print(); 
+    }
+}
